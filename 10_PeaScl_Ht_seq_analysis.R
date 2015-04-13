aca7c05411aeab54d77ac8be41989971d1860e4a
wjidea <peascl@chilvers-HPC>
2015-04-13 14:34:07 -0400

    write function for DEseq2 Calculate Contrast

--- 10_PeaScl_Ht_seq_analysis.R
+++ 10_PeaScl_Ht_seq_analysis.R
@@ -12,8 +12,7 @@ TimePoint <- c("12","12","24","24","48","48",
 ## DESeq data set needs to be a data.frame
 # SampleFile will connect independent files with the different conditions
 samplename <- unlist(lapply(sampleFiles,function(x) gsub("\\.htout\\.new","",x)))
-
-
+# prepare sample table
 sampleTable <- data.frame(sampleName=samplename,
                           fileName=sampleFiles,
                           Hosts=HostNames,
@@ -32,7 +31,7 @@ colData(ddsHTSeq)$Hosts <- factor(colData(ddsHTSeq)$Hosts, levels=c("Media_CK","
 colData(ddsHTSeq)$TP <- factor(colData(ddsHTSeq)$TP, levels=c("12","24","48"))
 
 # Normalize reads among gene expression
-dds<-DESeq(ddsHTSeq)
+dds<-DESeq(ddsHTSeq,test = "Wald")
 
 #if relevel is needed
 #dds$Hosts <- relevel(dds$Hosts, "Media_CK")
@@ -88,9 +87,9 @@ plot(hclust(dist(t(assay(rld)))), labels=colData(rld)$TP)
 
 # log counts
 par(mfrow=c(1,1))
-plot(assay(rld)[,1], assay(rld)[,2], cex=.1)
+plot(assay(rld)[,1], assay(rld)[,3], cex=.1)
 
-# show results with the genes that are up regulated
+# show results with the genes that are up or down regulated
 resBigFC <- results(dds, altHypothesis="greaterAbs",pAdjustMethod = "BH",)
 resBigFCsort <- resBigFC[order(resBigFC$padj),]
 head(resBigFCsort)
@@ -99,19 +98,85 @@ abline(h=c(-1,1),lwd=5)
 
 
 # generate results with contrast
-res_lift_CK <- results(dds,contrast = list("HostsLifter.TP12", "HostsMedia_CK.TP12"),pAdjustMethod = "BH")
-res_lift_CKSort <- res_lift_CK[order(res_lift_CK$padj),]
-head(res_lift_CKSort,10)
+# res.contrast <- function =(var1,var2,var3,var4){
+# }
 
+####################### TIME Effect  ###########################
+# 12 - 24 time effect in host Lifter
+res_1 <- results(dds,contrast = list(
+                        c("TP12","HostsLifter.TP12"), 
+                        c("TP24","HostsLifter.TP24")),
+                        pAdjustMethod = "BH")
+res_1_Sort <- res_1[order(res_1$padj),]
+T12Lift <- head(res_1_Sort,50)
+Test_res <- Cal.contrast("HostsPI240515","HostsPI240515.TP24","HostsLifter","HostsLifter.TP24")
+# 24 - 48 time effect in host Lifter
+res_2 <- results(dds,contrast = list(
+  c("TP24","HostsLifter.TP24"), 
+  c("TP48","HostsLifter.TP48")),
+  pAdjustMethod = "BH")
+res_2_Sort <- res_2[order(res_2$padj),]
+T24Lift <- head(res_2_Sort,50)
+
+# 12 - 24 time effect in host PI
+res_3 <- results(dds,contrast = list(
+  c("TP12","HostsPI240515.TP12"), 
+  c("TP24","HostsPI240515.TP24")),
+  pAdjustMethod = "BH")
+res_3_Sort <- res_3[order(res_3$padj),]
+T12PI <- head(res_3_Sort,50)
+
+# 24 - 48 time effect in host PI
+res_4 <- results(dds,contrast = list(
+  c("TP24","HostsPI240515.TP24"), 
+  c("TP48","HostsPI240515.TP48")),
+  pAdjustMethod = "BH")
+res_4_Sort <- res_4[order(res_4$padj),]
+T24PI <- head(res_4_Sort,50)
+
+
+####################### HOST Effect  ###########################
+# Lifter PI HOST effect in TP12
+res_5 <- results(dds,contrast = list(
+  c("HostsPI240515","HostsPI240515.TP12"), 
+  c("HostsLifter","HostsLifter.TP12")),
+  pAdjustMethod = "BH")
+res_5_Sort <- res_5[order(res_5$padj),]
+PLTP12 <- head(res_5_Sort,50)
+
+# Lifter PI HOST effect in TP24
+res_6 <- results(dds,contrast = list(
+  c("HostsPI240515","HostsPI240515.TP24"), 
+  c("HostsLifter","HostsLifter.TP24")),
+  pAdjustMethod = "BH")
+res_6_Sort <- res_6[order(res_6$padj),]
+PLTP24 <- head(res_6_Sort,50)
+
+Cal.contrast <- function(var1, var2, var3, var4){
+  res_func <- results(dds, contrast = list(
+    c(var1,var2),c(var3,var4)
+  ),pAdjustMethod = "BH")
+  res_funcSort <- res_func[order(res_func$padj),]
+  head(res_funcSort,50)
+}
+
+Test_res <- Cal.contrast("HostsPI240515","HostsPI240515.TP24","HostsLifter","HostsLifter.TP24")
+
+
+
+
+draw_stripchart(res_1_Sort,3)
+
+cond <- with(colData(dds), factor(paste(Hosts,TP)))
+draw_stripchart <- function(x,n){
 # VERY good function to show gene expression level between samples 
 # Give name to each column in the stripchart
-cond <- with(colData(dds), factor(paste(Hosts,TP)))
 # obtain normalized read from the top DE genes 
 # [2] is the rank of the gene in the dds sorted result (i.e. resSort)
-k <- counts(dds,normalized=TRUE)[rownames(res_lift_CKSort)[5],]
+k <- counts(dds,normalized=TRUE)[rownames(x)[n],]
 par(mar=c(8,5,2,2))
 stripchart(log2(k + 1) ~ cond, method="jitter", vertical=TRUE, las=2,)
-
+}
 
 library("RColorBrewer")
 library("gplots")
