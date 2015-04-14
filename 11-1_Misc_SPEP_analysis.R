149403a461186e4d44c0892e5e0b03f67ab2d293
wjidea <peascl@chilvers-HPC>
2015-04-14 18:48:31 -0400

    Update with spep

--- 11-1_Misc_SPEP_analysis.R
+++ 11-1_Misc_SPEP_analysis.R
@@ -1,3 +1,25 @@
+# Dependencies
+library(DESeq2)
+library(Biobase)
+library(GenomicRanges)
+library(ggplot2)
+library(gplots)
+
+# Function to draw plot based on gene name and contrast group
+draw_genename <- function (resfunc,name){
+  topGene <- grep(pattern = name,x = rownames(resfunc),value=TRUE)
+  chartTitle <- paste("Gene Name: ",name)
+  gdata <- plotCounts(dds , gene=topGene, intgroup=c("Hosts","TP"), 
+                      returnData=TRUE,normalized = TRUE)
+  plot_gene <- ggplot(gdata, aes(x=TP, y=count,color=Hosts, group=Hosts)) + 
+    geom_point(position=position_jitter(width=0.1,height=0),size=3,shape=16) 
+  p <- plot_gene + labs(title = chartTitle) + stat_smooth(se=F,method="loess")
+  return(list(p,as.vector(unlist(strsplit(topGene,"\\|")))[2]))
+}
+
+
+
+
 spep <- read.csv("/home/peascl/scl_de/htout_STAR_mergeGTF/R_output/SPEP.csv",header = TRUE,)
 counts_normal <- as.data.frame(counts(dds,normalized=TRUE))
 firstrow <- as.list(rownames(counts_normal))
@@ -22,6 +44,7 @@ spepcountSubset <- subset(spepcountSort, select = -c(GeneID_all) )
 # legend("bottomright",legend = c("Media-CK","Lifter","PI"),pch=c(15,16,17))
 # legend("topright",legend=c("12","24","48"),col=c("Green","Red","Black"),pch=16)
 
+# df here is the DE genes that belong to secreted proteins
 df <- data.frame()
 for (i in rownames(T24Lift)){
   x<-as.vector(unlist(strsplit(i,"\\|")))[2]
@@ -30,16 +53,6 @@ for (i in rownames(T24Lift)){
     df <- df[order(df$Effector_candidate),]
   }
 }
+draw_genename(T24Lift,"SS1G_12024")
 
-draw_genename <- function (resfunc,name){
-  Gene <- name
-  topgene <- rownames(dds[rownames(dds) %in% ])
-  chartTitle <- paste("Gene Name: ",as.vector(unlist(strsplit(topGene,"\\|")))[2])
-  gdata <- plotCounts(dds , gene=topGene, intgroup=c("Hosts","TP"), 
-                      returnData=TRUE,normalized = TRUE)
-  plot_gene <- ggplot(gdata, aes(x=TP, y=count,color=Hosts, group=Hosts)) + 
-    geom_point(position=position_jitter(width=0.1,height=0),size=3,shape=16) 
-  p <- plot_gene + labs(title = chartTitle) + stat_smooth(se=F,method="loess")
-  return(list(p,as.vector(unlist(strsplit(topGene,"\\|")))[2]))
-}
 
